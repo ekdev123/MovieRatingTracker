2e60a2ce804194229df285e41162ce3c67412231
Eldar <doles-verbals.0d@icloud.com>
2025-03-23 17:04:09 -0700

    First commit

---
+++ MovieRatingTracker/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main ()
+    {
+        string movieName;
+        
+        // impelemnting logic to accept and test user input
+        do
+        {
+            Console.WriteLine("Enter your movie name"); 
+            movieName = Console.ReadLine(); 
+        
+            //checking to make sure input is not empty
+            if (string.IsNullOrEmpty(movieName)) 
+            {
+                Console.WriteLine("Invalid entry please try again, the movie name can not be empty");
+            }
+       
+        } while (string.IsNullOrEmpty(movieName));
+        
+        Console.WriteLine("You entered,"+ movieName);
+
+    }
+}
